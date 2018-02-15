76ec0ec51f3e98850f25d96d9ff45805bf2c4c52
Anton Nemtsev <thesilentimp@gmail.com>
2018-02-15 20:56:48 +0200

    lock

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = () => ({
+  plugins: {
+    'postcss-import': true,
+    'postcss-mixins': true,
+    'postcss-simple-vars': true,
+    'postcss-conditionals': true,
+    'postcss-nested': true,
+    'postcss-color-function': true,
+    autoprefixer: true,
+  },
+});
